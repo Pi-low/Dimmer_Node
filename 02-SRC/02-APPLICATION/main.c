014e99655e11ceda1f1959cbaccc0458905e45c2
Chommanivong Nello <nello.chom@protonmail.com>
2021-03-21 21:52:57 +0100

    Initiate project

---
+++ 02-SRC/02-APPLICATION/main.c
@@ -0,0 +1,8 @@
+#include <xc.h>
+#include <stdint.h>
+#include "../01-GLOBALS/common.h"
+#include "main.h"
+
+void main (void) {
+    
+}
---
+++ 02-SRC/04-DRIVER/MCU_config.h
@@ -0,0 +1,8 @@
+#ifndef MCU_CONFIG_H
+#define	MCU_CONFIG_H
+
+#include <xc.h>
+
+
+#endif	/* MCU_CONFIG_H */
+
